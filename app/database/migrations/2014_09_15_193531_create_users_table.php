bc1703a77883a3e406ba43e89ef3c434d02c564c
james <tw.jameslee@gmail.com>
2014-11-01 17:58:41 +0800

    test

--- app/database/migrations/2014_09_15_193531_create_users_table.php
+++ app/database/migrations/2014_09_15_193531_create_users_table.php
@@ -5,31 +5,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $table)
-		{
-			$table->increments('id');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table)
+        {
+            $table->increments('id');
             $table->string('username');
             $table->string('email');
             $table->string('password');
-			$table->timestamps();
-		});
-	}
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
 
 }
